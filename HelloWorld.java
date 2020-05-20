ff766d6c4279989facc262d95ecb7c900eee204a
Andrew R Leonardi <andrewrleonardi@Andrews-MacBook-Air.local>
2020-05-20 11:19:33 -0700

    1_Hello World in the Command Line_Step 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
-    
     public static void main(String[] args) {
-        System.out.println("Hello World! It's me Kathryn!");
+        // Example of modifying the program; this reqires recompilation using the command javac HelloWorld.java 
+        System.out.println("Hello World! It's me Kathryn and I love TACOS!");
     }
 }

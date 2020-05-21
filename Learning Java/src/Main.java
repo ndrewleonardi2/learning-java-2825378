6f1dc2a128b59c5e99aae44e35d6013b78a7dbf3
Andrew R Leonardi <ndrewleonardi2@gmail.com>
2020-05-21 15:09:15 -0700

    2_Input and Output in Java

--- Learning Java/src/Main.java
+++ Learning Java/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         double studentGPA = 3.45;
@@ -5,5 +7,13 @@ public class Main {
         String studentLastName = "Hammon";
 
         System.out.println(studentFirstName + " " + studentLastName + " has a GPA of: " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        //Creates an instance of scanner class to access stud in
+        Scanner input = new Scanner(System.in);
+
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " NOW has a GPA of: " + studentGPA);
     }
 }

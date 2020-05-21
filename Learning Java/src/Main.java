fa2027899335bd7aa7c9fb97659ea466e8145034
Andrew R Leonardi <ndrewleonardi2@gmail.com>
2020-05-21 14:43:26 -0700

    2_Using indexes with Strings

--- Learning Java/src/Main.java
+++ Learning Java/src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);

cd9437b275dc5c34b81b10adbe211b98e9af3411
Andrew R Leonardi <ndrewleonardi2@gmail.com>
2020-05-21 16:30:25 -0700

    4_Debugging syntax and logical errors

--- Learning Java/src/Main.java
+++ Learning Java/src/Main.java
@@ -1,36 +1,37 @@
-import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String args[]) {
         String question = "What is the largest planet in our solar system?";
         String choiceOne = "earth";
         String choiceTwo = "jupiter";
         String choiceThree = "saturn";
+
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
         System.out.println(question);
 
         // Write a print statement giving the answer choices
-        System.out.println("1) "  + choiceOne);
-        System.out.println("2) "  + choiceTwo);
-        System.out.println("3) "  + choiceThree);
+        System.out.println("Choose one of the following: " +
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
 
-        // Prompt the user for input
+        // Have the user input an answer
         Scanner scanner = new Scanner(System.in);
-
         // Retrieve the user's input
         String input = scanner.next();
 
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user
-        if (correctAnswer.equals(input.toLowerCase())) {
-            System.out.println("Congrats. That's the correct answer!");
+        // then the user is correct and we want to print out a congrats message to the user.
+        if(correctAnswer.equals(input.toLowerCase())) {
+            System.out.println("Congrats! That's the correct answer");
         } else {
-            // If the user's input does not match the correctAnswer...
-            // then the user is incorrect and we want to print out a message to the user with the correct answer
-            System.out.println("Sorry, the correct answer is: " + correctAnswer);
+            System.out.println("You are incorrect. The correct answer is " + correctAnswer);
         }
+        // If the user's input does not match the correctAnswer...
+        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+
     }
+
 }
